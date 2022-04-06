e67fa7c96cc50cadd45b25aab971281a5a6bd5cf
Rushd Syed <rushd.syed@gmail.com>
2022-04-06 15:06:31 -0400

    updated SDK to 32, made new emulator

--- app/src/main/java/com/example/bp_fall_2021_quizapp/MainActivity.java
+++ app/src/main/java/com/example/bp_fall_2021_quizapp/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     // Put class variables up here
     // Best practice is to make them private (can only be accessed within the class, or using getters/setters)
     // Each UI component that you want to reference needs a variable
-
+    private EditText nameInput;
     /**
      * Method used to start an activity. It's the first method to run when the
      * activity begins
@@ -24,27 +24,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         // obtain user's name using findViewById
-
+        nameInput = findViewById(R.id.nameText);
     }
 
     /**
      * Get username here and open Quiz Questions
      * @param view
      */
-    public void startQuiz(View view){
+    public void startQuiz(View view) {
+
+
         // set name variable every time user clicks "start"
-        String userName = (String) findViewById(R.id.nameText).toString();
+        String userName = nameInput.toString();
         // If the name field is empty, prompt user to enter name
-        if(userName.equals("") || userName.equals("Name")){
+        if (userName.equals("") || userName.equals("Name")) {
             Toast.makeText(getBaseContext(), "Please input your name", Toast.LENGTH_SHORT).show();
         }
         // If user has entered name, begin quiz
-
-        Intent intent = new Intent(this, QuizQuestionActivity.class);
-        startActivity(intent);
-        finish(); // close current activity
+        else {
+            Intent intent = new Intent(this, QuizQuestionActivity.class);
+            startActivity(intent);
+            finish(); // close current activity
+            }
         }
-
     }
